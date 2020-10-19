653d51f735c1e84894ab4c18e8c28b58b1d6fabb
BogdanBercea <bogdan.bercea98@yahoo.com>
2020-10-19 11:20:18 +0300

    added an idea for the backend

---
+++ src/States/PublisingOnTheInternet.java
@@ -0,0 +1,14 @@
+package States;
+
+import java.util.concurrent.TimeUnit;
+
+public class PublisingOnTheInternet {
+
+    public PublisingOnTheInternet(){
+
+    }
+
+    public void run() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(3);
+    }
+}
---
+++ src/States/WriteDocumentation.java
@@ -0,0 +1,14 @@
+package States;
+
+import java.util.concurrent.TimeUnit;
+
+public class WriteDocumentation {
+
+    public WriteDocumentation(){
+
+    }
+
+    public void run() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(5);
+    }
+}
